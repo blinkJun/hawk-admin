0981eda0ad84870216c416d6c1fe4d4127db7ec9
liangjun <530080147@qq.com>
2023-04-25 11:45:52 +0800

    优化权限指令

--- src/directives/permissions.ts
+++ src/directives/permissions.ts
@@ -2,11 +2,12 @@
  * @Author liangjun
  * @LastEditors liangjun
  * @Date 2021-06-07 10:23:22
- * @LastEditTime 2023-04-18 16:49:51
+ * @LastEditTime 2023-04-25 11:45:10
  * @Description 验证权限方法和全局权限指令
  */
-import {VNode} from 'vue'
-import {useAccountStore} from '../store/account'
+import { VNode } from 'vue'
+import { useAccountStore } from '../store/account'
+import { ElMessage } from 'element-plus'
 
 const safeAuthCode = 'system'
 
@@ -16,19 +17,22 @@ const safeAuthCode = 'system'
  * @param {String} authCode 授权码  'sys:admins:create'
  * @return {Boolean}
  */
- export const validateAuthCode = function (authCode:string):boolean {
+export const validateAuthCode = function (authCode: string): boolean {
     const accountStore = useAccountStore()
     const funcAuth = accountStore.userAuthList
-    return authCode===safeAuthCode||(funcAuth && funcAuth.includes(authCode))
+    return authCode === safeAuthCode || (funcAuth && funcAuth.includes(authCode))
 }
-export const validateAuthCodeAsync = async function (authCode:string,latest?:boolean):Promise<boolean> {
+export const validateAuthCodeAsync = async function (authCode: string, latest?: boolean): Promise<boolean> {
     const accountStore = useAccountStore()
-    if(latest||accountStore.userAuthList.length===0){
+    if (latest || accountStore.userAuthList.length === 0) {
         await accountStore.initUserAuthState()
     }
     return validateAuthCode(authCode)
 }
 
+const noAuthMessage = ()=>{
+    ElMessage.error('您没有权限执行此操作！请联系管理员处理')
+}
 /**
  * @method handleAuthElement
  * @description 对进行权限管理的元素进行处理
@@ -36,30 +40,55 @@ export const validateAuthCodeAsync = async function (authCode:string,latest?:boo
  * @param {String} authCode  'sys:admins:create'
  * @return
  */
-export const handleAuthElement = function (el:HTMLElement, authCode:string) {
+export const handleAuthElement = function (el: HTMLElement, authCode: string, vnode: VNode, modifiers: any) {
+    const del = modifiers.del
     if (validateAuthCode(authCode)) {
+        // 移除隐藏元素样式
+        if (del) {
+            el.classList.remove('is-remove')
+        }
+        // 移除禁用样式
         el.removeAttribute('disabled')
         el.classList.remove('is-disabled')
+        // 恢复点击事件
+        const onClickHandle = vnode.el!._vei?.onClick
+        if(onClickHandle){
+            el.removeEventListener('click',noAuthMessage)
+            el.addEventListener('click',onClickHandle)
+        }
     } else {
+        // 使用样式隐藏元素
+        if (del) {
+            el.classList.add('is-remove')
+        }
+        // 添加禁用样式
         el.setAttribute('disabled', 'true')
         el.classList.add('is-disabled')
+        // 覆盖点击事件
+        const onClickHandle = vnode.el!._vei?.onClick
+        if(onClickHandle){
+            el.removeEventListener('click',onClickHandle)
+            el.addEventListener('click',noAuthMessage)
+        }
     }
 }
 
 export default {
-    install:function (app:any) {
+    install: function (app: any) {
         const authElements = new Map()
 
         // 权限指令 v-auth="sys:admin:create"
-        app.directive('auth', function (el:HTMLElement, binding:any, vnode:VNode) {
+        app.directive('auth', function (el: HTMLElement, binding: any, vnode: VNode) {
             const type = binding.arg
             const value = binding.value
-            handleAuthElement(el, value)
+            const modifiers = binding.modifiers
+            handleAuthElement(el, value, vnode, modifiers)
             authElements.set(vnode.key, {
                 type,
                 value,
                 el,
-                vnode
+                vnode,
+                modifiers
             })
         })
 
@@ -72,9 +101,9 @@ export default {
         const update = function () {
             console.log('update auth status')
             authElements.forEach(item => {
-                const { el, value } = item
+                const { el, value, vnode, modifiers } = item
                 if (el) {
-                    handleAuthElement(el, value)
+                    handleAuthElement(el, value, vnode, modifiers)
                 }
             })
         }
